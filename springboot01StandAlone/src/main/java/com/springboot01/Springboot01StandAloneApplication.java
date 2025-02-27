ca0ba038d684da7083075757c8898f81886c7425
Vivek Mishra <vivekmishra172003@gmail.com>
2025-02-27 21:32:19 +0530

    Add springboot01StandAlone project files

---
+++ springboot01StandAlone/src/main/java/com/springboot01/Springboot01StandAloneApplication.java
@@ -0,0 +1,14 @@
+package com.springboot01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01StandAloneApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Application started...");
+		SpringApplication.run(Springboot01StandAloneApplication.class, args);
+	}
+
+}
