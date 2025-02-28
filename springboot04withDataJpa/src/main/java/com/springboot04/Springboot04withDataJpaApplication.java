26484954944db5773861fff3c332f9060be2ec54
Vivek Mishra <vivekmishra172003@gmail.com>
2025-02-28 13:54:02 +0530

    Add springboot04withDataJpa project files

---
+++ springboot04withDataJpa/src/main/java/com/springboot04/Springboot04withDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.springboot04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot04withDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot04withDataJpaApplication.class, args);
+	}
+
+}
