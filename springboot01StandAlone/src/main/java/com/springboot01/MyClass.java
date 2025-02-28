babab66542c72430cec793abdc767bf792e11a0a
Vivek Mishra <vivekmishra172003@gmail.com>
2025-02-28 09:20:54 +0530

    Add springboot01StandAlone project files

---
+++ springboot01StandAlone/src/main/java/com/springboot01/MyClass.java
@@ -0,0 +1,9 @@
+package com.springboot01;
+
+
+
+public class MyClass {
+	public void printMessage(String name) {
+		System.out.println("name:"+name);
+	}
+}
--- springboot01StandAlone/src/main/java/com/springboot01/Springboot01StandAloneApplication.java
+++ springboot01StandAlone/src/main/java/com/springboot01/Springboot01StandAloneApplication.java
@@ -1,14 +1,28 @@
 package com.springboot01;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Springboot01StandAloneApplication {
+public class Springboot01StandAloneApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		System.out.println("Application started...");
 		SpringApplication.run(Springboot01StandAloneApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		myClass().printMessage("vivek");
+		
+	}
+	@Bean
+	public MyClass myClass() {
+		return new MyClass();
+	}
+ 
 }
+ 
