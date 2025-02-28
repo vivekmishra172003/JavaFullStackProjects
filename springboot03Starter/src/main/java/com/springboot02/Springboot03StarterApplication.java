dbaa2179f838456522179b2bbe9fdce8752ef390
Vivek Mishra <vivekmishra172003@gmail.com>
2025-02-28 10:04:54 +0530

    Add springboot03Starter project file created bean and done it directly

--- springboot03Starter/src/main/java/com/springboot02/Springboot03StarterApplication.java
+++ springboot03Starter/src/main/java/com/springboot02/Springboot03StarterApplication.java
@@ -8,21 +8,13 @@ import org.springframework.context.annotation.Bean;
 import com.springboot02.bean.Student;
 
 @SpringBootApplication
-public class Springboot03StarterApplication implements CommandLineRunner {
+public class Springboot03StarterApplication {
 
 	public static void main(String[] args)  {
 		SpringApplication.run(Springboot03StarterApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		stdBean().displayStdDetails();
-		
-	}
-	@Bean
-	public Student stdBean() {
-		return new Student("viek",12,28.2f);
-	}
+
+
 
 }
