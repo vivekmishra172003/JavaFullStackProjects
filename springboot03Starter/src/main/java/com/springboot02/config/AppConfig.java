197d04a6ba5fd86861c40a282b4cd0989a7845f6
Vivek Mishra <vivekmishra172003@gmail.com>
2025-03-01 13:06:44 +0530

    Add changes to springboot04withDataJpa

--- springboot03Starter/src/main/java/com/springboot02/config/AppConfig.java
+++ springboot03Starter/src/main/java/com/springboot02/config/AppConfig.java
@@ -1,5 +1,27 @@
 package com.springboot02.config;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.springboot02.bean.Student;
+
+@Configuration
 public class AppConfig {
+	@Bean
+	public CommandLineRunner cmdLineRunner() {
+		return  new CommandLineRunner() {
+			
+			@Override
+			public void run(String... args) throws Exception {
+				// TODO Auto-generated method stub
+				stdBean().displayStdDetails();
+			} 
+		};
+	}
+	@Bean
+	public Student stdBean() {
+		return new Student("viek",12,28.2f);
+	}
 
 }
