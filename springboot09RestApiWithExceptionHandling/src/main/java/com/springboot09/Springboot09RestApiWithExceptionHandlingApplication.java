fe3871eafd8d2027240dd7a45b295531cf5e88e2
Vivek Mishra <vivekmishra172003@gmail.com>
2025-03-08 09:59:04 +0530

    setup is done

---
+++ springboot09RestApiWithExceptionHandling/src/main/java/com/springboot09/Springboot09RestApiWithExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.springboot09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot09RestApiWithExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot09RestApiWithExceptionHandlingApplication.class, args);
+	}
+
+}
---
+++ springboot09RestApiWithExceptionHandling/src/test/java/com/springboot09/Springboot09RestApiWithExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot09RestApiWithExceptionHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
