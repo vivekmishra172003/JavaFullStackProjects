458476baa2634beadbfbc0c82474ad6f922a2643
Vivek Mishra <vivekmishra172003@gmail.com>
2025-03-10 10:43:22 +0530

    Spring data  basic setup is done

---
+++ springboot11library-management/src/main/java/com/springboot10/Springboot11libraryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.springboot10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot11libraryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot11libraryManagementApplication.class, args);
+	}
+
+}
---
+++ springboot11library-management/src/test/java/com/springboot10/Springboot11libraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot11libraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
