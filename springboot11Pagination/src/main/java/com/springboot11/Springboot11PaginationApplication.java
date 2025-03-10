171e18b50e89258c9914b24bfe15e95108b009d1
Vivek Mishra <vivekmishra172003@gmail.com>
2025-03-10 11:33:33 +0530

    basic setup is done for pagination

---
+++ springboot11Pagination/src/main/java/com/springboot11/Springboot11PaginationApplication.java
@@ -0,0 +1,13 @@
+package com.springboot11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot11PaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot11PaginationApplication.class, args);
+	}
+
+}
---
+++ springboot11Pagination/src/test/java/com/springboot11/Springboot11PaginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot11PaginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
