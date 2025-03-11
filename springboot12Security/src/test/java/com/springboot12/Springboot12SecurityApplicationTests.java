e7eec66566a08471dc5e06ea40861cbe8c971482
Vivek Mishra <vivekmishra172003@gmail.com>
2025-03-11 13:23:30 +0530

    Spring auth basic setsup is done

---
+++ springboot12Security/src/test/java/com/springboot12/Springboot12SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot12SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
