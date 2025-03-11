60363f815b9a22bdcaecc15c452078338fe45e7d
Vivek Mishra <vivekmishra172003@gmail.com>
2025-03-11 17:30:29 +0530

    basic setup for basic example is done

---
+++ springboot16SimpleOauth2/src/main/java/com/springboot16/Springboot16SimpleOauth2Application.java
@@ -0,0 +1,13 @@
+package com.springboot16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot16SimpleOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot16SimpleOauth2Application.class, args);
+	}
+
+}
---
+++ springboot16SimpleOauth2/src/test/java/com/springboot16/Springboot16SimpleOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot16SimpleOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
