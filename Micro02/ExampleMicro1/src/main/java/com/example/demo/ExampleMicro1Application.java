68bc52b1d631d3d0ab9c4ac59ed054793dec01c0
Vivek Mishra <vivekmishra172003@gmail.com>
2025-03-15 20:33:10 +0530

    Add Micro02 project files

---
+++ Micro02/ExampleMicro1/src/main/java/com/example/demo/ExampleMicro1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleMicro1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleMicro1Application.class, args);
+	}
+
+}
---
+++ Micro02/ExampleMicro1/src/test/java/com/example/demo/ExampleMicro1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleMicro1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
