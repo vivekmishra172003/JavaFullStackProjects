91368544c3cb40a2f20b4d4bc8c87bbd1fcb2de8
Vivek Mishra <vivekmishra172003@gmail.com>
2025-03-19 16:28:46 +0530

    Add springboot18Rabbitmq files

---
+++ springboot18Rabbitmq/src/main/java/com/springboot18/Springboot18RabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.springboot18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot18RabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot18RabbitmqApplication.class, args);
+	}
+
+}
---
+++ springboot18Rabbitmq/src/test/java/com/springboot18/Springboot18RabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot18RabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
