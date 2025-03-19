fd791b28630cb12f866f32dd9ab2c61fb468c3ff
Vivek Mishra <vivekmishra172003@gmail.com>
2025-03-19 16:33:01 +0530

    Add springboot19Rabbitmq02 Baisc project setup is done and added depencey  rabbitmq for srping and spring web

---
+++ springboot19Rabbitmq02/src/main/java/com/rabbitmqdemo/Springboot19Rabbitmq02Application.java
@@ -0,0 +1,13 @@
+package com.rabbitmqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot19Rabbitmq02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot19Rabbitmq02Application.class, args);
+	}
+
+}
