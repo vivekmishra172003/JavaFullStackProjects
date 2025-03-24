b3b2477911923ce7193ffd39b127ffffc0148056
Vivek Mishra <vivekmishra172003@gmail.com>
2025-03-24 12:06:11 +0530

    Add Fin02 deals-coupons-app files and basic setup for parnet dpencyy  is done

---
+++ Fin02/deals-coupons-app/src/main/java/com/itransform/DealsCouponsAppApplication.java
@@ -0,0 +1,13 @@
+package com.itransform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealsCouponsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealsCouponsAppApplication.class, args);
+	}
+
+}
---
+++ Fin02/deals-coupons-app/src/test/java/com/itransform/DealsCouponsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itransform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealsCouponsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
