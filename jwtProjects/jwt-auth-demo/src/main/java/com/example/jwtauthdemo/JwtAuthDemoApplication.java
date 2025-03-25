8ed04dcec322a680b13c81369b4e747bff363779
vivekmishra172003 <vivekmishra172003@gmail.com>
2025-03-25 12:13:51 +0530

    Add initilze fisrt proejcts

---
+++ jwtProjects/jwt-auth-demo/src/main/java/com/example/jwtauthdemo/JwtAuthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtauthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthDemoApplication.class, args);
+	}
+
+}
